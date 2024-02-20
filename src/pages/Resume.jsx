b73428b4c4a0b64e0563156c21f2c89485832392
brutchley <bhutchison0901@yahoo.com>
2024-02-19 19:56:34 -0600

    added portfolio links with corresponding images

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,11 +3,11 @@ export default function Resume() {
       <div>
         <h1>Resume</h1>
         <div id="resume-points">
-          <h4 id="eac">Education and Certifications</h4>
+          <h4 id="eac">Education and Certifications:</h4>
           <li id="bio">The University of Kansas (B.A. Biology), 2015</li>
           <li id="coding">KU Coding Boot Camp Graduate, 2024</li>
           <li id="haacp">HAACP Certified, 2023</li>
-          <h4 id="plac">Programming Languages & Concepts</h4>
+          <h4 id="plac">Programming Languages & Concepts:</h4>
           <li id="javascript">JavaScript</li>
           <li id="css">CSS</li>
           <li id="html">HTML</li>
